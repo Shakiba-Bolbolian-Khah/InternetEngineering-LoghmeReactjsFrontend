36e1cd8d78f570f067b5aa05f03b89763d0ea6bf
Mir Hamed <h.jafar77@gmail.com>
2020-06-11 23:44:07 +0430

    Frontend test 1

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import Home from "./containers/HomeContainers/Home"
 import Authentication from "./containers/Authentication"
 
+console.log("ghable if 1")
+console.log(localStorage.getItem("JWT"))
 if (localStorage.getItem("JWT") !== null) {
+    console.log("bade if 1")
+    console.log(localStorage.getItem("JWT"))
     var now = new Date();
     var expDate = new Date(localStorage.getItem("expDate"));
     if(now.getTime() <= expDate.getTime()) {
+        console.log("bade if 2")
+        console.log(localStorage.getItem("JWT"))
         ReactDOM.render(<Home type={"normal"}/>, document.getElementById("root"));
     }
     else {

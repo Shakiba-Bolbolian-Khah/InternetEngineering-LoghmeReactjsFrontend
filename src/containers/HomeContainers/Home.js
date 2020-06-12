c93311da87486419441c4ab4e60771e7865e15be
Mir Hamed <h.jafar77@gmail.com>
2020-06-12 13:42:52 +0430

    Frontend test 6

--- src/containers/HomeContainers/Home.js
+++ src/containers/HomeContainers/Home.js
@@ -30,6 +30,8 @@ class Home extends React.Component {
     componentDidMount() {
         var now = new Date();
         var expDate = new Date(localStorage.getItem("expDate"));
+        console.log(now)
+        console.log(expDate)
         if(now.getTime() >= expDate.getTime()) {
             localStorage.removeItem("JWT")
             localStorage.removeItem("expDate")
